da16a0d3ca76583a5a1ff06ea4caa5f2637cfb7a
RiccardoZappitelli <zappitelliriccardo@gmail.com>
2026-02-04 16:05:28 +0100

    removed old unused constants

--- utils/commands.py
+++ utils/commands.py
@@ -8,39 +8,3 @@ class Command:
     description: str
     category: str   # emoji + category
     label: str     # friendly button label for menus
-
-
-category_order = [
-    "🏠 Main Menu",
-    "🛑 System",
-    "🌐 Network",
-    "📸 Camera",
-    "🔊 Audio",
-    "🎵 Sound FX",
-    "😈 Pranks",
-    "🦑 Misc",
-    "💻 System Control",
-    "🎮 Input",
-    "📋 Messaging",
-    "🔒 Can't Open",
-    "🧠 Keylogger",
-    "🔧 Utility",
-    "🕵️‍♂️ MITM"
-]
-
-CATEGORY_TO_MENU = {
-    "🛑 System": "menu_system",
-    "🌐 Network": "menu_network",
-    "📸 Camera": "menu_camera",
-    "🔊 Audio": "menu_audio",
-    "🎵 Sound FX": "menu_soundfx",
-    "😈 Pranks": "menu_pranks",
-    "💻 System Control": "menu_control",
-    "🎮 Input": "menu_input",
-    "📋 Messaging": "menu_messaging",
-    "🔒 Can't Open": "menu_cantopen",
-    "🧠 Keylogger": "menu_keylogger",
-    "🦑 Misc": "menu_misc",
-    "🔌 PlugIns": "menu_plugins",
-    "🕵️‍♂️ MITM": "menu_mitm",
-}
