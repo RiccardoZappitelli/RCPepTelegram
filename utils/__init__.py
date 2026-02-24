47149cd9012523bec5cfd022e03bfab2d3c3cef4
RiccardoZappitelli <zappitelliriccardo@gmail.com>
2026-02-24 16:10:55 +0100

    added create_startup_task

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,11 @@
 from .system_interaction.cmdsession import CMDSession
 from .system_interaction.mymixer import CustomMixer
 from .system_interaction.overlays import OpenCVOverlayPlayer, OverlayManager, invert_image, distorted_screen, user_prompt
-from .program_system.tunnel_handler import *
 from .system_interaction.network_utils import *
 from .system_interaction.audio_player import *
+from .system_interaction.make_self_as_task import create_startup_task
+
+from .program_system.tunnel_handler import *
 from .program_system.telegram_widgets import *
 from .program_system.messages import *
 from .program_system.general import *
@@ -12,6 +14,7 @@ from .program_system.cancellable_thread import *
 from .program_system.commands import Command
 
 from .user_interaction.chat import *
+from .user_interaction.notifications import notify_toast, notify_toast_with_url, AUDIO_MAP as WinotifyAudioMap
+
 from .input_injection.duckyscript import *
 from .input_injection.mouse_controller import *
-from .user_interaction.notifications import notify_toast, notify_toast_with_url, AUDIO_MAP as WinotifyAudioMap
--- utils/system_interaction/__init__.py
+++ utils/system_interaction/__init__.py
@@ -2,4 +2,5 @@ from .audio_player import *
 from .cmdsession import *
 from .mymixer import *
 from .network_utils import *
-from .overlays import *
+from .overlays import OpenCVOverlayPlayer, OverlayManager
+from .make_self_as_task import create_startup_task
