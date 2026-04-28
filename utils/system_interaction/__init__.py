b44720e711667a0c4a38f6b01566f714136faf60
RiccardoZappitelli <zappitelliriccardo@gmail.com>
2026-04-28 16:45:17 +0200

    __init__.py

--- utils/system_interaction/__init__.py
+++ utils/system_interaction/__init__.py
@@ -3,4 +3,4 @@ from .cmdsession import *
 from .mymixer import *
 from .network_utils import *
 from .overlays import OpenCVOverlayPlayer, OverlayManager
-from .make_self_as_task import create_startup_task
+from .setstartup import create_startup_task
